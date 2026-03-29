055e355959f584de20710e919c8671a6f4295bde
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-03-29 21:34:04 +0530

    classes and objects changes

--- classesandobjects.py
+++ classesandobjects.py
@@ -1,10 +1,15 @@
 class Human:
     def __init__(self):
         self.name = "Shahid"
+        
     def Say(self):
         print("Hello!")
 
 
 shahid = Human()
-
+shahid.name = "dumb"
+print(shahid.name)
 shahid.Say()
+
+x = Human()
+print(x.name)
