1085731ba0aa12ab937cf96d3684418599416aa6
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-03-30 16:51:45 +0530

    Changes have been made!

--- classesandobjects.py
+++ classesandobjects.py
@@ -1,15 +1,28 @@
-class Human:
-    def __init__(self):
-        self.name = "Shahid"
-        
-    def Say(self):
-        print("Hello!")
+class student:
+    def __init__ (self):
+        NameOfStd = "Student didnt register his name"
+        regnum = "Student didnt register his register number"
+    def display(self):
+        print("Name: ", self.NameOfStd)
+        print("Register Number: ", self.regnum)
+S1 = student()
+S2 = student()
+
+S1.NameOfStd = "Shahid"
+S2.NameOfStd = "Anwershah"
+S1.regnum = "5237"
+S2.regnum = "6218"
+
+print(S1.NameOfStd)
+print(S2.NameOfStd)
+print(S1.regnum)
+print(S2.regnum)
+
+S1.display()
+S2.display()
 
 
-shahid = Human()
-shahid.name = "dumb"
-print(shahid.name)
-shahid.Say()
 
-x = Human()
-print(x.name)
+
+
+
