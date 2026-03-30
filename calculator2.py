eff563fe3dc0c4ecebea4a7ed8c8d619c42a054f
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-03-30 18:55:05 +0530

    Simple Calculator

---
+++ calculator2.py
@@ -0,0 +1,39 @@
+
+class calculator:
+
+    def __init__(self, a, b):
+        self.num1 = a
+        self.num2 = b 
+
+    def add(self):
+
+        print("Addition result = ", self.num1 + self.num2 )
+
+    def sub(self):
+
+        print("Subraction result = ", self.num1 - self.num2 )
+
+    def mul(self):
+
+        print("Multiplication result = ", self.num1 * self.num2 )
+
+    def div(self):
+
+        print("Divison result = ", self.num1 / self.num2 )
+        
+object = calculator(8,5)
+
+object.add()
+object.sub()
+object.mul()
+object.div()
+
+
+
+
+
+        
+
+
+
+
