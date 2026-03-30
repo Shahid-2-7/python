c82edb0b436659b2cbd357a2f1b6a4b20eca8197
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-03-30 17:53:42 +0530

    changes have been made!

--- classesandobjects.py
+++ classesandobjects.py
@@ -1,30 +1,22 @@
 class student:
 
-    def __init__ (self):
+    def __init__ (self): 
+
         self.NameOfStd = "Student didnt register his name"
         self.regnum = "Student didnt register his register number"
 
     def display(self):
+
         print("Name: ", self.NameOfStd)
         print("Register Number: ", self.regnum)
 
 S1 = student()
 S2 = student()
 
-print(S1.NameOfStd)
-print(S2.NameOfStd)
-print(S1.regnum)
-print(S2.regnum)
-
 S1.NameOfStd = "Shahid"
 S2.NameOfStd = "Anwershah"
 S1.regnum = "5237"
 S2.regnum = "6218"
-
-print(S1.NameOfStd)
-print(S2.NameOfStd)
-print(S1.regnum)
-print(S2.regnum)
 
 S1.display()
 S2.display()
@@ -32,6 +24,7 @@ S2.display()
 #below example explains a different method of changing values for objects in a class:
 
 class fruit:
+
     def __init__(self, col):
         self.colour = col
         
@@ -40,4 +33,23 @@ orange = fruit("orange")
 kiwi = fruit("green")
 papaya = fruit("yellow")
 
-print(apple.colour, orange.colour, kiwi.colour, papaya.colour, end = "")
+print(apple.colour, orange.colour, kiwi.colour, papaya.colour)
+
+class teacher:
+
+    def __init__ (self, m, n):
+        self.tname = m
+        self.treg = n
+    def disteach(self):
+        print("name: ", self.tname)
+        print("register number: ", self.treg)
+
+bala = teacher("Bala", 6543)
+surya = teacher("Surya", 4328)
+
+bala.disteach()
+surya.disteach()
+
+
+
+    
