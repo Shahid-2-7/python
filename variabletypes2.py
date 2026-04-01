db028d36cbc04871123ce2258cce2decc5feaacf
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-04-01 17:16:25 +0530

    changes have been made to 'variabletypes2.py'

--- variabletypes2.py
+++ variabletypes2.py
@@ -9,14 +9,19 @@ class laptop:
         self.price = price
         print("Price:", price)
          
-    @classmethod
+    @classmethod  #this is called decoraters this one is used to call a function in a class with the class itself
     def display(cls):
         cls.chargertype = "C-Type"
         print("Charger type changed to c-type.")
 
+    @staticmethod  #this decorater is used to access a function without keyword self
+    def information():
+        print("Class = laptop")
+
 hp = laptop()
 hp.setprice(20000)
 laptop.display()
+hp.information()
 
 
 
