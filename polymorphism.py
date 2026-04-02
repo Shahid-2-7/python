5cd1bb3ca816681ad252a59cb478edd9131cb56a
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-04-02 18:25:34 +0530

    Polymorphism types and usage

--- polymorphism.py
+++ polymorphism.py
@@ -5,3 +5,28 @@ add(2,2)
 add(2,2,2)
 
 #the word polymorphism means 'having many forms'. in context to programing, it is used to use a function with same name but with different 'signatures'
+
+class an():
+    def sn(self):
+        print("Animals make sound")
+
+class dog(an):
+    def sn(self):
+        print("Dog Barks")
+
+class bird(an):
+    def sn(self):
+        print("Birs Sings")
+
+class cat(an):
+    def sn(self):
+        print("Cat Meows")
+
+a = an()
+a.sn() 
+a1 = dog()
+a1.sn()
+a2 = bird()
+a2.sn()
+a3 = cat()
+a3.sn()
