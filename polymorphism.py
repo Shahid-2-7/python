7668f5c30333cfbe46c971f91b3373356b91245d
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-04-02 18:17:09 +0530

    Polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,7 @@
+def add(a,b,c = 0):
+    print(a+b+c)
+
+add(2,2)
+add(2,2,2)
+
+#the word polymorphism means 'having many forms'. in context to programing, it is used to use a function with same name but with different 'signatures'
