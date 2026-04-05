2ee63208cb847295ed23851771cb24c208b0e184
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-04-05 20:34:03 +0530

    polymorphism and inheritence using programns

--- randomlol.py
+++ randomlol.py
@@ -16,5 +16,25 @@ class student(person):
 obj = student("Shahid","A")
 obj.display()
 
+class employee():
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+class manager(employee):
+    def __init__(self,name,salary,department):
+        super().__init__(name,salary)
+        self.department = department
+
+    def display(self):
+         print("Name of employee:",self.name)
+         print("Salary of employee:",self.salary)
+         print("Department of employee:",self.department)
+        
+        
+e1 = manager("shahid", "35,000", "AI Tools development")
+e1.display()
+
+
 
 
