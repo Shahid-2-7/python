4bf5374d99de38c9404e17ecf5bf3fbb06db5464
Nowfal Thajudeen <shahidroblox27@gmail.com>
2026-04-06 20:12:15 +0530

    First Commit

--- encapsulation.py
+++ encapsulation.py
@@ -7,6 +7,7 @@ class a():
 
 o1 = a()
 o1.dis()
-        
+ #'__' make a variable uncustomizable outside the class. 
+ #if you try to edit it outside the class, it will show an error that the class doesnt have that attribute
 
 
